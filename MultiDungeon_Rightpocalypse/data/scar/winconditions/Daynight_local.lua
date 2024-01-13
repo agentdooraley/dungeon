04c0af5d082883e053ff9ad0e411ef7dde932d39
agentdooraley <nickit.grachev@yandex.ru>
2024-01-13 14:45:20 +0400

    2 commit

---
+++ MultiDungeon_Rightpocalypse/data/scar/winconditions/Daynight_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+	exclusive				= false,
+	victory_condition	= false,
+	always_on			= false,
+	title						= "Day and Night",
+	win_message 		= "",
+	lose_message 	= "",
+	description			= "The locale is governed by an individual (random) 'Day and Night' cycle, which affects vision."
+}
---
+++ MultiDungeon_Rightpocalypse/data/scar/winconditions/Weather_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+	exclusive			= false,
+	victory_condition	= false,
+	always_on			= false,
+	title				= "Weather Conditions",
+	win_message 		= "",
+	lose_message 		= "",
+	description			= "Adds random Weather Condition effects on the locale, that impact the armies accordingly."
+}
