c654eb854c0128e9d911696b717c2e03eac56705
agentdooraley <nickit.grachev@yandex.ru>
2024-01-24 23:09:53 +0400

    0.22a 24.01.24
    риперы  до конца убран скейл
    имморталы убран скейл по энергии
    селесты убран скейл по энергии
    ап точности хеллхаунда

---
+++ MultiDungeon_Rightpocalypse/data/scar/winconditions/addon_racethemes_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+	exclusive 			= false,
+	victory_condition 	= false,
+	always_on 			= false,
+	title 				= "$7500065",
+	win_message 		= "",
+	lose_message 		= "",
+	description 		= "$7500070"
+}
