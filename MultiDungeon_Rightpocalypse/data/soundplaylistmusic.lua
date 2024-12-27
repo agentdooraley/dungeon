3dc2ccaa863c64d413c5402e556008e1dae89303
agentdooraley <nickit.grachev@yandex.ru>
2024-12-27 17:28:01 +0400

    0.22a 27.12.24 huita 2

---
+++ MultiDungeon_Rightpocalypse/data/soundplaylistmusic.lua
@@ -0,0 +1,30 @@
+playlist = 
+{
+	tracks = 
+	{		
+		"music_imperial_guard_theme",
+		"music_msd1",
+		"music_msd1_perc_choir",
+		"music_msd2",
+		"music_msd2_perc_choir",
+		"music_msd3_perc",
+		"music_msd4_perc_choir",
+		"music_msd5o",
+		"music_mso1_perc_choir",
+		"music_mso2",
+		"music_mso2_perc_choir",
+		"music_mso3",
+		"music_mso4_perc",
+		"music_EldarThemev2",
+		"music_theme_chaos_01",
+		"music_ork_theme",
+		"music_coming_of_chaos",
+		"music_force_commander_theme",
+	},
+	
+	
+	silence_min = 20.0,
+	silence_max = 50.0,
+	
+	order = false,
+}
