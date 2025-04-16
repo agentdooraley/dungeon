35aac394ffc9b389b12f63ee44132bdbcd81f933
agentdooraley <nickit.grachev@yandex.ru>
2025-04-16 18:28:37 +0400

    0.29 12.04.25 веспиды урон переделан дпс по лоу зданиям с 12 до 18 дпс по хай демонам с минимального до 24 дпс по остальным юнитам без изменений брат хп с 1500 до 1400 брат реген в бою с 1 до 2 соб серафимки реинфорс с 55/5 до 50/5 соб серафимки вывод с 220/20 до 200/20 сервитор хп с 400 до 350 все лп1 потеряли 200 хп кана выход с 60 до 45 кап рейт миссионеров с 0.63 до 0.7 гварда кд гранат с 4.8 до 4 ПРОФЕССОР УРФИН ДЖЮС — 1:01 дестр мс с 24 до 28 некрон турка цена с 125 до 110 ПРОФЕССОР УРФИН ДЖЮС — 1:45 хеви дестр вывод с 50 до 40 хеви дестр цена с 475 до 375 аннигиляционная баржа цена с 770 до 500 паук цена с 480 до 440 лорд дестр цена с 350 до 250 лорд дестр вывод с 90 до 60с убраны лишние грейды из термака некрона барракута перемещена в т2 барракуда цена с 210/360 до 125/320 ионная пушка барракуда кд с 0.15 до 0.35 тау урон мисл подов барракуды с 440-364 до 400-200 барракуда пробитие по лоу-мед зданиям снижено на 10% нл переделан урон артефактов на стрельбу и мили мельта лорда хаоса теперь аналогична мельте лысого дальность мельт лысого и лорда с 25 до 22 грейд на броню техники теперь работает на всех дестров кс закачка на плазму теперь имеет урон аналогичный плазме хсм кс все члены пачки получили 25хп кс лидер хп с 500 до 550 кс псайкер теперь получил пассивку обычного псайкера дальники ветераны снижен урон от пинка на 50% ветераны с кемпой выход с 360/120 до 320/120 ветераны с кемпой убран 20% бонус урона ветераны с кемпой мили заменено на мили от ветеранов без бонуса к урону найтбрингер мс с 24 до 28 найтспиннер лимит 2 маркердрон пастов переделан, теперь доступен в т3 монтки, снижает время перезарядки метки в пачке на 50%, повышает обзор на 5 ед

---
+++ MultiDungeon_Rightpocalypse/data/scar/winconditions/0_gametesting_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+exclusive = false,
+victory_condition = false,
+always_on = false,
+title = "[DEV] Testing Mode",
+win_message = "",
+lose_message = "",
+description = "[SET TO OFF !!] Used exclusively for TESTING purposes.  Allows immediate purchase of all game's addons/researches/squads/buildings.  Combine with CHANGE PLAYERS to test all players.   USE WITH CAUTION!"
+}
